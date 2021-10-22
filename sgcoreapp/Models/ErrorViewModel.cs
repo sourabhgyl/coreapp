48931f3cb6853fe1ea8a1ca117f9d061db4776d9
sourabh goyal <sogoyal@live.com>
2021-10-22 21:13:37 +0800

    first commit

---
+++ sgcoreapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sgcoreapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
